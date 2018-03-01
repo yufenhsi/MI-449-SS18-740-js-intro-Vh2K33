37611c21b38b1f7c0611e11e7588199285a703b3
Yufen <hsiyufen@msu.edu>
2018-03-01 16:16:30 -0500

    remove test functions and change variable names

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 
-var buttonElement = document.getElementById('button1');
-var audio1 = document.getElementById('my-audio1')
-buttonElement.addEventListener('mouseenter', function() {
-  console.log('I was clicked!')
-  audio1.play()
+var hithatButton = document.getElementById('hithat-button');
+var hithatAudio = document.getElementById('hithat-audio')
+hithatButton.addEventListener('click', function() {
+  hithatAudio.play()
+});
+hithatButton.addEventListener('mouseenter', function() {
+  hithatAudio.play()
 });
 
-var buttonElement = document.getElementById('button2');
-var audio2 = document.getElementById('my-audio2')
-buttonElement.addEventListener('mouseenter', function () {
-  console.log('Do Something!')
-  audio2.play()
+var clapButton = document.getElementById('clap-button');
+var clapAudio = document.getElementById('clap-audio')
+clapButton.addEventListener('click', function () {
+  clapAudio.play()
+});
+clapButton.addEventListener('mouseenter', function () {
+  clapAudio.play()
 });
 
-var buttonElement = document.getElementById('button3');
-var audio3 = document.getElementById('my-audio3')
-buttonElement.addEventListener('mouseenter', function () {
-  console.log('Hello!')
-  audio3.play()
+var kickButton = document.getElementById('kick-button');
+var kickAudio = document.getElementById('kick-audio')
+kickButton.addEventListener('click', function () {
+  kickAudio.play()
+});
+kickButton.addEventListener('mouseenter', function () {
+  kickAudio.play()
 });
 
-var buttonElement = document.getElementById('button4');
-var audio4 = document.getElementById('my-audio4')
-buttonElement.addEventListener('mouseenter', function () {
-  console.log('Help!!')
-  audio4.play()
-})
+var tomButton = document.getElementById('tom-button');
+var tomAudio = document.getElementById('tom-audio')
+tomButton.addEventListener('click', function () {
+  tomAudio.play()
+});
+tomButton.addEventListener('mouseenter', function () {
+  tomAudio.play()
+});
